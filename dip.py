23aca7cf8ecb6a1edf5525f9203364d87ab30bd1
Sergey-Gorb <mpost39228@mail.ru>
2021-09-10 19:10:52 +0300

    second commit

--- dip.py
+++ dip.py
@@ -11,24 +11,60 @@ class VKpop:
         self.token = token
         self.user_id = ''
         self.url = 'https://api.vk.com'
-        self.method = 'photos.getAll'
+        self.method = 'photos.get'
+        self.method_album = 'photos.getAlbums'
+        self.method_users = 'users.get'
+        self.params_users = 'user_ids'
+        self.method_utils = 'utils.resolveScreenName'
+        self.params_utils = 'screen_name'
         self.params = 'extended=1&photo_sizes=1&v=5.77'
         self.dict_photo = []
 
-    def get_photo_info(self, offset=0, count=10):
-        params = f'user_id={self.user_id}&{self.params}&access_token={self.token}&count={count}&offset={offset}'
+    def set_user_id(self, user_input):
+        if user_input.isdigit():
+            params = f'user_ids={user_input}&fields=name'
+            s_req = f'{self.url}/method/{self.method_users}?{params}&access_token={self.token}&v=5.77'
+            res = requests.get(s_req)
+            data_user = json.loads(res.text)
+            if str(data_user['response'][0]['id']) == user_input:
+                self.user_id = data_user['response'][0]['id']
+                return True
+            else:
+                return
+        else:
+            params = f'screen_name={user_input}'
+            s_req = f'{self.url}/method/{self.method_utils}?{params}&access_token={self.token}&v=5.77'
+            res = requests.get(s_req)
+            data_user = json.loads(res.text)
+            if data_user['response']['type'] == 'user':
+                self.user_id = data_user['response']['object_id']
+                return True
+            else:
+                return
+
+    def get_photo_info(self, album_id, offset=0, count=10):
+        params = f'user_id={self.user_id}&album_id={album_id}&{self.params}&access_token={self.token}' \
+                 f'&count={count}&offset={offset}'
         s_req = f'{self.url}/method/{self.method}?{params}'
         res = requests.get(s_req)
-        print(res.text)
         return json.loads(res.text)
 
-    def get_photo(self, user_id, path_to_save, number_photo=5):
-        self.user_id = user_id
+    def get_album_info(self):
+        params = f'user_id={self.user_id}&{self.params}&access_token={self.token}'
+        s_req = f'{self.url}/method/{self.method_album}?{params}'
+        res = requests.get(s_req)
+        return json.loads(res.text)
+
+    def get_photo(self,  path_to_save, number_photo=5):
         i_off = 0
         i_count = 10
         list_of_photos = []
-        while True:
-            data = self.get_photo_info(offset=i_off, count=i_count)
+        data_album = self.get_album_info()
+        for album in data_album['response']['items']:
+            i_off = 0
+            i_count = 10
+            album_id = album['id']
+            data = self.get_photo_info(album_id, offset=i_off, count=i_count)
             if not data['response']['items']:
                 break
             for files in data['response']['items']:
@@ -53,8 +89,6 @@ class VKpop:
                 list_of_photos.append(cur_photo)
             i_off += i_count
         list_of_photos.sort(key=operator.itemgetter(2), reverse=True)
-        print(list_of_photos[:number_photo])
-        print(len(list_of_photos))
         for i in range(number_photo):
             self.dict_photo.append({'file_name': list_of_photos[i][0], 'size': list_of_photos[i][1]})
             api = requests.get(list_of_photos[i][4])
@@ -75,12 +109,24 @@ class YaUploader:
         self.headers = {'Content-Type': 'application/json', 'Accept': 'application/json',
                         'Authorization': f'OAuth {token}'}
 
-    def upload(self, file_path, list_photo):
+    def set_dest_path(self,save_path):
+        """Метод проверяет наличие папки save_path и, при необходимости создает ее"""
+        res = requests.get(f'{self.url}?path=%2F{save_path}&preview_crop=true', headers=self.headers)
+        if res.status_code == 404:
+            resp = requests.put(f'{self.url}?path=%2F{save_path}', headers=self.headers)
+            if resp.status_code == 201:
+                return True
+            else:
+                return False
+        elif res.status_code == 200:
+            return True
+
+    def upload(self, source_path, save_path, list_photo):
         """Метод загружает файлы по списку list_photo на яндекс диск"""
         replace = True
         for photo in list_photo:
-            file_spec = file_path / photo['file_name']
-            yd_spec = photo['file_name']
+            file_spec = source_path / photo['file_name']
+            yd_spec = save_path + '/' + photo['file_name']
             res = requests.get(f'{self.url}/upload?path={yd_spec}&overwrite={replace}', headers=self.headers).json()
             with open(file_spec, 'rb') as f:
                 try:
@@ -93,9 +139,18 @@ class YaUploader:
 if __name__ == '__main__':
     token_vk = '...'
     downloader = VKpop(token_vk)
-    vk_user = '670053379'
-    path_dir = Path.cwd() / 'images'
-    file_list = downloader.get_photo(vk_user, path_dir)
-    token_yd = '...'
-    uploader = YaUploader(token_yd)
-    uploader.upload(path_dir, file_list)
+    vk_user = input('Enter <user_id> or <screen_name> for VK member: ')
+    if downloader.set_user_id(vk_user):
+        path_dir = Path.cwd() / 'images'
+        vk_count_photo = int(input('Enter count of photo saved from VK member albums: '))
+        file_list = downloader.get_photo( path_dir, vk_count_photo)
+        token_yd = '...'
+        uploader = YaUploader(token_yd)
+        yd_save_folder = input('Enter YD folder name for saved photo: ')
+        if uploader.set_dest_path(yd_save_folder):
+            uploader.upload(path_dir, yd_save_folder,file_list)
+            print('Done')
+        else:
+            print(f'Problem with folder for saved photo <{yd_save_folder}>')
+    else:
+        print(f'Problem with VK member <{vk_user}>')
